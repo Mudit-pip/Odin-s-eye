db380f5fe77d6ea7d9edcb2ff76ec9fd5ee25556
ccell <70972592+Mudit-pip@users.noreply.github.com>
2025-01-21 15:28:49 +0530

    Updated Image paths

--- app/src/main/java/com/odinseyenavigation/E5_inside_quest.java
+++ app/src/main/java/com/odinseyenavigation/E5_inside_quest.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +15,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -173,38 +180,66 @@ public class E5_inside_quest extends AppCompatActivity {
         }
     }
 
-    private void setimageondisplay(String queimagename) {
-        storageReference = FirebaseStorage.getInstance().getReference("queimages/" + queimagename + ".jpg");
-        try {
-            File localfile = File.createTempFile("tempfile", ".jpg");
-            storageReference.getFile(localfile)
-                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                            if (pg.isShowing()) {
-                                pg.dismiss();
-                            }
+//    private void setimageondisplay(String queimagename) {
+//        storageReference = FirebaseStorage.getInstance().getReference("queimages/" + queimagename + ".jpg");
+//        try {
+//            File localfile = File.createTempFile("tempfile", ".jpg");
+//            storageReference.getFile(localfile)
+//                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+//                            if (pg.isShowing()) {
+//                                pg.dismiss();
+//                            }
+//
+//                            Bitmap bitmap = BitmapFactory.decodeFile(localfile.getAbsolutePath());
+//                            questionimage.setImageBitmap(bitmap);
+//
+//
+//                        }
+//                    }).addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception e) {
+//                            if (pg.isShowing()) {
+//                                pg.dismiss();
+//                            }
+//                            Toast.makeText(E5_inside_quest.this, "Error loading image", Toast.LENGTH_SHORT).show();
+//
+//                        }
+//                    });
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
 
-                            Bitmap bitmap = BitmapFactory.decodeFile(localfile.getAbsolutePath());
-                            questionimage.setImageBitmap(bitmap);
+    private void setimageondisplay(String imageUrl) {
+        pg.show();  // Show progress dialog before loading
 
-
+        Glide.with(this)
+                .load(imageUrl)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        if (pg.isShowing()) {
+                            pg.dismiss();  // Dismiss progress dialog
                         }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            if (pg.isShowing()) {
-                                pg.dismiss();
-                            }
-                            Toast.makeText(E5_inside_quest.this, "Error loading image", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(E5_inside_quest.this, "Error loading image", Toast.LENGTH_SHORT).show();
+                        return false;  // Return false to allow Glide to handle further processing
+                    }
 
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        if (pg.isShowing()) {
+                            pg.dismiss();  // Dismiss progress dialog once image is loaded
                         }
-                    });
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+                        return false;  // Return false to allow Glide to set the image on ImageView
+                    }
+                })
+                .into(questionimage);
     }
+
+
 
     private String sanitizeEmail(String email) {
         return email != null ? email.replace(".", "_dot_").replace("@", "_at_") : "error";
